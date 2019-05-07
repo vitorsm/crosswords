ee1aa0ab5cd0353d66766567fe8ec1a589cd9e8f
Vítor Moreira <vitor.sousa.moreira@hotmail.com>
2019-05-06 22:06:36 -0300

    initial commit

---
+++ src/main/java/br/com/intcode/crosswords/CrosswordsApplication.java
@@ -0,0 +1,13 @@
+package br.com.intcode.crosswords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosswordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrosswordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/intcode/crosswords/CrosswordsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.intcode.crosswords;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrosswordsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
